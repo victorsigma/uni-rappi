021cf2a9d9baad2449f4ea291ab237143e5dabda
victorsigma <victorossielgaray1@gmail.com>
2024-09-18 22:41:16 -0600

    implementación de ionic

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'uni-rappi',
+  webDir: 'www'
+};
+
+export default config;
